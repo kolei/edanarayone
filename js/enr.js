f4a8bbaf0d835c5db079a0f987a4470a35c77afb
Евгений Колесников <kolei@ya.ru>
2021-02-03 16:27:57 +0300

    поменял тестовый хост

--- js/enr.js
+++ js/enr.js
@@ -1,4 +1,4 @@
-window.script_version = 13;
+window.script_version = 14;
 var tilda_form_id = 'form226638292'; //'form199889435';
 var DEV_MODE = true;
 
@@ -278,7 +278,7 @@ $(document).ready(function ()
         tilda_form_id = 'form199889435';
     }
     else 
-        window.CHAIHONA_HOST = 'https://tilda.dev.chaihona1.ru';
+        window.CHAIHONA_HOST = 'https://wwwmk.dev.chaihona1.ru';
 
     console.log('v1.%s, CHAIHONA_HOST = %s', window.script_version, window.CHAIHONA_HOST);
 
